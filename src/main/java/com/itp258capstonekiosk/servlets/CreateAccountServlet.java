4a74c833b8a56d8e964dc8be960ed39e963df98d
GcRayden <gc.rayden@gmail.com>
2024-04-09 19:46:15 -0400

    Added Account Creation

--- src/main/java/com/itp258capstonekiosk/servlets/CreateAccountServlet.java
+++ src/main/java/com/itp258capstonekiosk/servlets/CreateAccountServlet.java
@@ -2,11 +2,16 @@ package com.itp258capstonekiosk.servlets;
 
 import java.io.IOException;
 
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import com.itp258capstonekiosk.services.AccountService;
 
 /**
  * Servlet implementation class CreateAccountServlet
@@ -15,16 +20,21 @@ import javax.servlet.http.HttpServletResponse;
 public class CreateAccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Resource(name = "jdbc/japanese")
+	private DataSource dataSource;
+
     /**
-     * Default constructor.
+     * @see HttpServlet#HttpServlet()
      */
     public CreateAccountServlet() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
@@ -33,8 +43,20 @@ public class CreateAccountServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        AccountService account = new AccountService(dataSource);
+        account.createAccount(username, password, "user");
+
+        // Send to JSP page
+ 		RequestDispatcher dispatcher = request.getRequestDispatcher("/public/index.jsp");
+ 		dispatcher.forward(request, response);
+
 		doGet(request, response);
 	}
 
