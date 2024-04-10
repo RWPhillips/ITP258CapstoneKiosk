e8c82d9b015134bb2241b3b5ce8aa087a0ec3c45
GcRayden <gc.rayden@gmail.com>
2024-04-10 00:30:57 -0400

    Fixed database login

--- src/main/java/com/itp258capstonekiosk/services/KioskDbUtil.java
+++ src/main/java/com/itp258capstonekiosk/services/KioskDbUtil.java
@@ -37,8 +37,8 @@ public class KioskDbUtil {
         }
 
         String jdbcUrl = "jdbc:mysql://localhost/kioskdatabase?useUnicode=true&characterEncoding=UTF-8";
-        String user = "itp246";
-        String password = "itp246";
+        String user = "kioskadmin";
+        String password = "wCMDP3$0h2A%";
 
         try {
             connection = DriverManager.getConnection(jdbcUrl, user, password);
