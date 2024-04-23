259373914cba028ae8f6b43e2aadf3a32ef9c2ea
GcRayden <adequateprogrammer@gmail.com>
2024-04-23 18:33:18 -0400

    Added CartServlet
    Returns Sub Item from Database

--- src/main/java/com/itp258capstonekiosk/services/CartService.java
+++ src/main/java/com/itp258capstonekiosk/services/CartService.java
@@ -3,8 +3,11 @@ package com.itp258capstonekiosk.services;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
+
+import com.itp258capstonekiosk.objects.SubItemObject;
 
 public class CartService {
 	
@@ -18,6 +21,40 @@ public class CartService {
 		dataSource = theDataSource;
 	}
 	
+	public SubItemObject getSubItem(String name) {
+		
+        SubItemObject subItem = new SubItemObject();
+		
+		try {
+			// Connect to the database
+	        database = new KioskDbUtil(dataSource);
+	        connection = database.getConnection();
+	        
+	        // Prepare the SQL statement to get all items
+	        CallableStatement callableStatement = connection.prepareCall("{CALL getSubItemList(?)}");
+	        callableStatement.setString(1, name);
+	        
+	        // Execute the query
+	        resultSet = callableStatement.executeQuery();
 	
+	        // Check for sub item
+	        if (resultSet.next()) {
+		        String subItemName = resultSet.getString(1);
+		        double subItemCost = resultSet.getDouble(2);
+		        int subItemCat = resultSet.getInt(3);
+		                       
+		        // Create ItemObject instance
+		        subItem = new SubItemObject(subItemName);
+	        }
+        
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    } finally {
+	        // Close JDBC objects
+	        database.closeConnection(connection, callableStatement, resultSet);
+	    }
+		
+        return subItem;
+	}
 
 }
