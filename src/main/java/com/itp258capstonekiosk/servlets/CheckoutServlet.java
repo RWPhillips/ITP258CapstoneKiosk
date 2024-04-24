bba752f61d797bbdbb47f3ec0f269439a6628c87
Daniel Juilfs <dcj84944@email.vccs.edu>
2024-04-24 12:36:19 -0400

    Create CheckoutServlet.java

---
+++ src/main/java/com/itp258capstonekiosk/servlets/CheckoutServlet.java
@@ -0,0 +1,90 @@
+package com.itp258capstonekiosk.servlets;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import com.itp258capstonekiosk.objects.ItemObject;
+import com.itp258capstonekiosk.objects.SubItemObject;
+import com.itp258capstonekiosk.services.CartService;
+
+/**
+ * Servlet implementation class CheckoutServlet
+ */
+@WebServlet("/CheckoutServlet")
+public class CheckoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Resource(name = "jdbc/kioskdatabase")
+	private DataSource dataSource;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public CheckoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		// Get object
+		HttpSession session = request.getSession();
+		ItemObject item = (ItemObject) session.getAttribute("orderItem");
+		
+		
+		CartService cart = new CartService(dataSource); 
+		
+		// Get strings
+		String[] subItems = request.getParameterValues("subitems");
+		
+
+		if (subItems != null) {
+			// Make sub-object array
+			ArrayList<SubItemObject> subList = new ArrayList<SubItemObject>();
+		
+			// Go through each string, create a new sub object with the string, and then store it into sub-object array
+			for (String i : subItems) {
+				
+				
+				SubItemObject newSub = cart.getSubItem(i);
+				subList.add(newSub);
+			}
+		
+			// Add all sub items to the item
+			item.setSubItems(subList);
+		}
+		ArrayList<ItemObject> items = (ArrayList<ItemObject>)session.getAttribute("cart");  
+		if (items == null ) {
+			items = new ArrayList<ItemObject>(); 
+		}
+		
+		items.add(item); 
+
+		// Store in the cart session header
+		session.setAttribute("cart", items);
+		
+		request.getRequestDispatcher("secure/ManageCart.jsp").forward(request, response); 
+	}
+		
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
